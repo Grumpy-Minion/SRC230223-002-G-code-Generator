0d9a3ebd222cbb8a0c2d1abc754a022d814900c5
Grumpy-Minion <edmund_tam@themedtechlab.com>
2023-03-11 16:33:06 +0800

    added deug statements to spiral surface data frame function.

--- SRC230223-002 Rev01-01-01-05 G-code Generator (Redlined).py
+++ SRC230223-002 Rev01-01-01-05 G-code Generator (Redlined).py
@@ -12,7 +12,7 @@
 # rev: 01-01-01-05
 # date: 11/Mar/2023
 # description:
-# Added error text file statements to spiral boss function.
+# Added error text file statements to spiral boss, spiral drill, spiral surface dataframe function.
 # update error checks using abort function in spiral boss function.
 #
 # rev: 01-01-01-04
@@ -1644,6 +1644,10 @@ def spiral_surface(origin_x, origin_y, start_dia, end_dia, doc, dia, step, z_f, 
     # text = G-code text
 
     # ---Change History---
+    # rev: 01-01-01-05
+    # update error checks using abort function.
+    # software test run on 11/Mar/2023
+    #
     # rev: 01-01-10-07
     # changed from writing G-code directly to txt file to a separate text variable.
     # software test run on 04/04/2022
@@ -1683,15 +1687,17 @@ def spiral_surface(origin_x, origin_y, start_dia, end_dia, doc, dia, step, z_f, 
 
     # check if safe_z is above surface.
     if safe_z <= 0:
-        print(f'''!!script aborted!!\nspiral_surface\nsafe_z below surface\nsafe_z = {"%.4f" % safe_z}''')
+#        print(f'''!!script aborted!!\nspiral_surface\nsafe_z below surface\nsafe_z = {"%.4f" % safe_z}''')
 #        text = f'''\n(!!script aborted!!)\n(safe_z below surface)\n(safe_z = {"%.4f" % safe_z})\n'''  # write header for section.
-        quit()
+#        quit()
+        abort('safe_z', safe_z, 'safe_z below surface')
 
     # check if start dia is larger than tool dia.
     if end_dia < dia:
-        print(f'''!!script aborted!!\nspiral_surface\nhole dia is larger tool dia.\nend dia = {"%.4f" % end_dia}\ntool dia = {"%.4f" % dia}''')
+#        print(f'''!!script aborted!!\nspiral_surface\nhole dia is larger tool dia.\nend dia = {"%.4f" % end_dia}\ntool dia = {"%.4f" % dia}''')
 #        text = f'\n(!!script aborted!!)\n(end dia is larger tool dia)\n'  # write header for section.
-        quit()
+#        quit()
+        abort('end_dia', end_dia, f'end dia is smaller than tool dia.\nend dia = {"%.4f" % end_dia}\ntool dia = {"%.4f" % dia}')
 
     # initialize variables
     length = start_dia/2 - dia/2
@@ -2851,7 +2857,7 @@ def spiral_drill_data_frame(name, excel_file, sheet):
 
     # ---Change History---
     # rev: 01-01-01-05
-    # initial release
+    # Added error text file statements to spiral drill function.
     # software test run on 11/Mar/2023
     #
     # rev: 01-01-10-09
@@ -2914,6 +2920,10 @@ def spiral_surface_data_frame(name, excel_file, sheet):
     # N/A
 
     # ---Change History---
+    # rev: 01-01-01-05
+    # Added error text file statements to spiral surface dataframe function.
+    # software test run on 11/Mar/2023
+    #
     # rev: 01-01-10-09
     # initial release
     # software test run on 14/Apr/2022
@@ -2924,9 +2934,16 @@ def spiral_surface_data_frame(name, excel_file, sheet):
     counter = 0             # initialize counter
     text = ''             # initialize
 
+    text_debug = f'\n{operation}\n'\
+                 f'{sheet}\n'\
+                 f'total rows: {rows}\n\n'
+    text_debug = indent(text_debug, 8)
+    write_to_file(name_debug, text_debug)  # write to debug file
+
     while counter <= last_row:
 
         # import parameters from excel file.
+        last_row_flag = format_data_frame_variable(df, 'last_row_flag', counter)
         origin_x = format_data_frame_variable(df, 'origin_x', counter)
         origin_y = format_data_frame_variable(df, 'origin_y', counter)
         start_dia = format_data_frame_variable(df, 'start_dia', counter)
@@ -2936,12 +2953,21 @@ def spiral_surface_data_frame(name, excel_file, sheet):
         cut_f = format_data_frame_variable(df, 'cut_f', counter)
         finish_f = format_data_frame_variable(df, 'finish_f', counter)
         finish_cuts = format_data_frame_variable(df, 'finish_cuts', counter)
-        safe_z = clear_z
+        safe_z = format_data_frame_variable(df, 'safe_z', counter)
+
+        text_debug = f'row: {counter}, last_row_flag: {last_row_flag}, origin_x: {origin_x}, origin_y: {origin_y}, start_dia: {start_dia}, end_dia: {end_dia}, doc: {doc}, step: {step}, cut_f: {cut_f}, finish_f: {finish_f}, finish_cuts: {finish_cuts}, safe_z: {safe_z}\n'
+        text_debug = indent(text_debug, 8)
+        write_to_file(name_debug, text_debug)  # write to debug file
 
         # generate G-code
         text_temp = spiral_surface(origin_x, origin_y, start_dia, end_dia, doc, dia, step, z_f, cut_f, finish_f, finish_cuts, safe_z, name)
-
         text = text + text_temp
+
+        break_flag, text_temp = last_row_detect(df, sheet, last_row_flag, last_row, counter, 8)  # detect last row
+        if break_flag == True:  # break if last row
+            text = text + text_temp
+            break
+
         counter = counter + 1  # increment counter.
 
     write_to_file(name, text)
